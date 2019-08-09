40700302b0d0b96d78628b9b7e547429e77e4bf0
sweetbeard30 <bludd1one@gmail.com>
2019-08-09 15:11:50 +0200

    tests

--- Visitor.js
+++ Visitor.js
@@ -1,40 +1,31 @@
 const fs = require('fs');
 class Visitor{
-    constructor(name, age, comments, assistor){
+    constructor(name, age, time, date, comments, assistor){
         this.name = name;
         this.age = age;
-        this.date = this.dateTime().date;
-        this.time = this.dateTime().time;
+        this.date = date;
+        this.time = time;
         this.comments = comments;
         this.assistor = assistor;
     }
-    dateTime(){
-        let dt = new Date,
-            humanReadable = dt.toUTCString();
-        return {
-            date: humanReadable.substring(0,16),
-            time: humanReadable.substring(17)
-        }
-    }
     save(){
-        let serialised = JSON.stringify(this, null, 2);
-        fs.readdir("./uploads", (err,files) => {
+        let test = JSON.stringify(this, null, 2);
+        fs.readdir("./uploads", function(err,files){
             if(err) throw new Error(err)
-            fs.writeFile(`./uploads/visitor_${files.length + 1}.json`, serialised, (err2, saved)=>{
+            fs.writeFile(`./uploads/visitor_${files.length + 1}.json`, test, function(err2, saved){
                 if(err2) throw new Error(err2)
-                console.log('Saved!')
             });
         });
     }
     load(arg){
-        fs.readFile(`./uploads/visitor_${arg}.json`, (err,data) => {
+        fs.readFile(`./uploads/visitor_${arg}.json`, function(err,data){
             if(err) throw new Error(err);
             let obj = JSON.parse(data)
-            console.log(obj.name)
             return obj.name;
         })
     }
 }
-let visitor = new Visitor("Asanda", 21, "blah blah blah blah blah", "Sephula");
-//visitor.save();
-visitor.load(2)
+let visitor = new Visitor("Asanda", 21, "14:40", "01-09-2019", "blah blah blah blah blah", "Chanel");
+visitor.save();
+// visitor.load(1)
+module.exports = Visitor;
